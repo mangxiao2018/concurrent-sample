40a4dd57e4d33e607050004791cdcb6f3f79b70a
mangxiao <mangxiao2018@126.com>
2021-05-06 14:20:40 +0800

    TestJoin

---
+++ concurrent-samples/src/main/java/com/mangxiao/concurrent/threads/TestJoin.java
@@ -0,0 +1,68 @@
+package com.mangxiao.concurrent.threads;
+
+import lombok.extern.slf4j.Slf4j;
+
+import static com.mangxiao.concurrent.util.Sleeper.sleep;
+
+/**
+ * TestJoin
+ */
+@Slf4j(topic = "c.TestJoin")
+public class TestJoin {
+
+    static int r = 0;
+    static int r1 = 0;
+    static int r2 = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        test3();
+    }
+
+    public static void test3() throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            sleep(2);
+            r1 = 10;
+        });
+        long start = System.currentTimeMillis();
+        t1.start();
+        log.debug("join begin");
+        t1.join(3000);
+        long end = System.currentTimeMillis();
+        log.debug("r1:{} r2:{} cost:{}",r1,r2,end - start);
+    }
+
+    public static void test2()throws InterruptedException{
+        Thread t1 = new Thread(()->{
+           sleep(1);
+           r1 = 10;
+        });
+        Thread t2 = new Thread(()->{
+            sleep(2);
+            r2 = 20;
+        });
+        t1.start();
+        t2.start();
+        long start = System.currentTimeMillis();
+        log.debug("join begin");
+        t2.join();
+        log.debug("t2 join end");
+        t1.join();
+        log.debug("t1 join end");
+        long end = System.currentTimeMillis();
+        log.debug("r1:{} r2:{} cost:{}",r1,r2,end - start);
+    }
+
+    private static void test1() throws InterruptedException {
+        log.debug("开始");
+        Thread t1 = new Thread(()->{
+            log.debug("开始");
+            sleep(1);
+            log.debug("开始");
+            r = 10;
+        });
+        t1.start();
+        t1.join();
+        log.debug("结果为:{}",r);
+        log.debug("结束");
+    }
+}
