d5b8f98ea8a44cb293723f0506e5bb9ce767ac8e
mangxiao <mangxiao2018@126.com>
2021-05-20 18:10:31 +0800

    TestCorrectPostureStep1

---
+++ concurrent-samples/src/main/java/com/mangxiao/concurrent/sync/TestCorrectPostureStep1.java
@@ -0,0 +1,46 @@
+package com.mangxiao.concurrent.sync;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "c.TestCorrectPostureStep1")
+public class TestCorrectPostureStep1 {
+    static final Object room = new Object();
+    static boolean hasCigarette = false;
+    static boolean hasTakeout = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(()->{
+            synchronized (room){
+                log.debug("has cigarette:", hasCigarette);
+                if (!hasCigarette){
+                    log.debug("have a rest");
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                log.debug("has cigarette:", hasCigarette);
+                if (hasCigarette){
+                    log.debug("go...");
+                }
+            }
+        },"xiao nan").start();
+
+        for (int i=0;i<5;i++){
+            new Thread(()->{
+                synchronized (room){
+                    log.debug("go...");
+                }
+            },"other").start();
+        }
+
+        Thread.sleep(1000);
+        new Thread(()->{
+            synchronized (room){
+                hasCigarette = true;
+                log.debug("cigarette");
+            }
+        },"give").start();
+    }
+}
