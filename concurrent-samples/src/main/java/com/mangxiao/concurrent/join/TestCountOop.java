175b8ff78c51d9231b0caa708b6366746024deeb
mangxiao <mangxiao2018@126.com>
2021-05-24 09:35:24 +0800

    TestCountOop

---
+++ concurrent-samples/src/main/java/com/mangxiao/concurrent/join/TestCountOop.java
@@ -0,0 +1,52 @@
+package com.mangxiao.concurrent.join;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "c.TestCountOop")
+public class TestCountOop {
+    public static void main(String[] args) throws InterruptedException {
+        Room room = new Room();
+        Thread t1 = new Thread(()->{
+            for (int j = 0; j<5000; j++){
+                room.increment();
+            }
+        },"t1");
+
+        Thread t2 = new Thread(()->{
+            for (int j = 0; j < 5000; j++){
+                room.decrement();
+            }
+        },"t2");
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+        log.debug("count:{}", room.get());
+    }
+
+
+}
+
+class Room{
+    int value = 0;
+    public void increment(){
+        synchronized (this){
+            value ++;
+        }
+    }
+
+    public void decrement(){
+        synchronized (this){
+            value --;
+        }
+    }
+
+    public int get(){
+        synchronized (this){
+            return value;
+        }
+    }
+}
+
