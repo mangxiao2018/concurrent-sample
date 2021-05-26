fcb068a40a0536e7d84d68113fac10406ff91ed7
mangxiao <mangxiao2018@126.com>
2021-05-26 16:03:19 +0800

    空

--- concurrent-samples/src/main/java/com/mangxiao/concurrent/sync/TestCounterSync.java
+++ concurrent-samples/src/main/java/com/mangxiao/concurrent/sync/TestCounterSync.java
@@ -7,7 +7,7 @@ public class TestCounterSync {
 
     static int counter = 0;
     static final Object room = new Object();
-
+    
     public static void main(String[] args) throws InterruptedException{
         Thread t1 = new Thread(()->{
             for (int i = 0;i<5000;i++){
