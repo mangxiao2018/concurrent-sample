4cc5f5ecc6f8144069a62944230a84a8e4d3d450
mangxiao <mangxiao2018@126.com>
2021-05-29 17:36:06 +0800

    TestThreadSafe

---
+++ concurrent-samples/src/main/java/com/mangxiao/concurrent/safe/TestThreadSafe.java
@@ -0,0 +1,63 @@
+package com.mangxiao.concurrent.safe;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+
+@Slf4j(topic = "c.TestThreadSafe")
+public class TestThreadSafe {
+    static final int THREAD_NUMBER = 2;
+    static final int LOOP_NUMBER = 200;
+    public static void main(String[] args){
+        ThreadSafeSubClass test = new ThreadSafeSubClass();
+        for (int i=0;i<THREAD_NUMBER;i++){
+            new Thread(()->{
+                test.method1(LOOP_NUMBER);
+            },"Thread" + (i+1)).start();
+        }
+    }
+}
+
+class ThreadUnsafe{
+    ArrayList<String> list = new ArrayList<>();
+    public void method1(int loopNumber){
+        for (int i=0;i<loopNumber;i++){
+            method2();
+            method3();
+        }
+    }
+    public void method2(){
+        list.add("1");
+    }
+
+    public void method3(){
+        list.add("0");
+    }
+}
+
+class ThreadSafe{
+    public final void method1(int loopNumber){
+        ArrayList<String> list = new ArrayList<>();
+        for (int i=0;i<loopNumber;i++){
+            method2(list);
+            method3(list);
+        }
+    }
+    public void method2(ArrayList<String> list){
+        list.add("1");
+    }
+
+    public void method3(ArrayList<String> list){
+        System.out.println(1);
+        list.remove(0);
+    }
+}
+
+class ThreadSafeSubClass extends ThreadSafe{
+    public void method3(ArrayList<String> list){
+        System.out.println(2);
+        new Thread(()->{
+            list.remove(0);
+        }).start();
+    }
+}
